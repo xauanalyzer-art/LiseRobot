464ec211b1f7fb5638f4b5aa63961019a8b96455
xauanalyzer-art <xauanalyzer@gmail.com>
2026-06-14 06:26:05 +0330

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 BOT_TOKEN = "8904903713:AAEV26b-NQp6pQqUSoYH7IdUZv2TJpciWjo"
 ADMIN_ID = 7665161693  # آیدی عددی ادمین (از @userinfobot بگیر)
 CHANNEL_USERNAME = "lisevpn"  # بدون @
-
+SUPPORT_USERNAME = "lisecontact" 
 # قیمت‌های پایه ماهانه (تومان) برای حجم‌های مختلف
 PRICES = {
     5: 150000,
@@ -10,3 +10,4 @@ PRICES = {
     15: 410000,
     20: 540000
 }
+
